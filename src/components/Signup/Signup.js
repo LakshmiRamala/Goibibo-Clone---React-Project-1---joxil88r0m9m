6bbb01d3bc10fe3eca54d937802f6336913e41f5
Lakshmi <lakshmiramala1425@gmail.com>
2023-11-19 15:42:06 +0530

    updates

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../provider/AuthProvider";
-import { JsxEmit } from "typescript";
 
 export default function SignUp() {
   const [message, setMessage] = useState("");
