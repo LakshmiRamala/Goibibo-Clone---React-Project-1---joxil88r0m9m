3613416f461db7dafd0cc9fe745c7db9f20e0452
Lakshmi <lakshmiramala1425@gmail.com>
2024-01-31 15:25:33 +0530

    changes

--- src/components/Navbar/Profile.js
+++ src/components/Navbar/Profile.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useAuth } from "../provider/AuthProvider";
-import {  Menu, MenuItem } from "@mui/material";
+import { Menu, MenuItem } from "@mui/material";
+
 export const Profile = () => {
   const navigate = useNavigate();
   const { isLoggedIn, setIsLoggedIn } = useAuth();
-  const userName = JSON.parse(sessionStorage.getItem("userName")).toUpperCase();
+  const userName = sessionStorage.getItem("userName") ? JSON.parse(sessionStorage.getItem("userName")).toUpperCase() : "";
   const [anchorEl, setAnchorEl] = useState(null);
-  const firstletter=isLoggedIn && (userName.charAt(0).toUpperCase());
+  const firstletter = isLoggedIn && userName.length > 0 ? userName.charAt(0).toUpperCase() : "";
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -24,39 +25,33 @@ export const Profile = () => {
   };
 
   const logout = () => {
-    sessionStorage.removeItem("userToken");
-    sessionStorage.removeItem("userName");
-    sessionStorage.removeItem("userEmail");
-    sessionStorage.removeItem("userPassword");
-    sessionStorage.removeItem("bookingId");
-    sessionStorage.removeItem("userId");
-    sessionStorage.removeItem("details");
-    sessionStorage.removeItem("hotelDetails");
+    sessionStorage.clear();
     setIsLoggedIn(false);
     navigateHandler("/login");
   };
 
   return (
     <section>
-    
-      <section onClick={handleClick}>
-       {window.innerWidth<=768 && ( <section className="profile-icon">
-          <AccountCircleIcon color="primary" fontSize="large" />
-        </section>)}
-        {window.innerWidth>768 && !isLoggedIn &&  (
-          <section className="profile-box">
-            <AccountCircleIcon color="primary" fontSize="large"/>
-            <h1>LOGIN / SIGNUP</h1>
-            </section>
+      <div onClick={handleClick}>
+        {window.innerWidth <= 768 && (
+          <div className="profile-icon">
+            <AccountCircleIcon color="primary" fontSize="large" />
+          </div>
         )}
-         {window.innerWidth>768 && isLoggedIn &&  (
-          <section className="profile-box">
+        {window.innerWidth > 768 && !isLoggedIn && (
+          <div className="profile-box">
+            <AccountCircleIcon color="primary" fontSize="large" />
+            <h1>LOGIN / SIGNUP</h1>
+          </div>
+        )}
+        {window.innerWidth > 768 && isLoggedIn && (
+          <div className="profile-box">
             <div className="login-icon">{firstletter}</div>
             <h1>{userName}</h1>
-            </section>
+          </div>
         )}
-        {window.innerWidth<=768 && isLoggedIn && <span>{userName}</span>}
-      </section>
+        {window.innerWidth <= 768 && isLoggedIn && <span>{userName}</span>}
+      </div>
 
       <Menu
         anchorEl={anchorEl}
@@ -66,8 +61,12 @@ export const Profile = () => {
         {isLoggedIn ? (
           <section>
             <MenuItem onClick={logout}>Logout</MenuItem>
-            <MenuItem onClick={() => navigateHandler("/mysupport/trips")}>Trips</MenuItem>
-            <MenuItem onClick={() => navigateHandler("/myProfile")}>My Profile</MenuItem>
+            <MenuItem onClick={() => navigateHandler("/mysupport/trips")}>
+              Trips
+            </MenuItem>
+            <MenuItem onClick={() => navigateHandler("/myProfile")}>
+              My Profile
+            </MenuItem>
           </section>
         ) : (
           <section>
