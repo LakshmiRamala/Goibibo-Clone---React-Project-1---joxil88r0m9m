d1dc01ae3658556a7624a0e9c35cad6d02becc47
Lakshmi <lakshmiramala1425@gmail.com>
2024-02-13 20:53:48 +0530

    changes

--- src/components/Trains/CheckoutTrainPage.js
+++ src/components/Trains/CheckoutTrainPage.js
@@ -52,6 +52,8 @@ export default function CheckoutTrainPage() {
       </div>
       <p>Baggage -7 Kgs (1 piece only) Cabin</p>
     </section>
+    </div>
+    <div>
     <section className="flight-price-details">
       <h1>FARE SUMMARY</h1>
       <span className="grand-total">
